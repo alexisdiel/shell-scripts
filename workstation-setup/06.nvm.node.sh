0adbd3c92f439ea0364ae8eb926f2e86eec3043a
Alexis Diel <alexisdiel@gmail.com>
2020-05-12 17:18:51 -0300

    install zsh

--- workstation-setup/06.nvm.node.sh
+++ workstation-setup/06.nvm.node.sh
@@ -1,10 +1,11 @@
 
 echo '########## <installing nvm> ##########'
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 source ~/.zshrc
+
 nvm --version
 nvm install 12
 nvm alias default 12
